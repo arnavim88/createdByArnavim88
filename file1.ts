8eaaa89b3d8edba93bd4d33cb6719c3cc1adc067
dorsho88 <dorshoham88@gmail.com>
2023-11-16 15:40:34 +0200

    Update file1.ts

--- file1.ts
+++ file1.ts
@@ -1,1 +1,13 @@
-"hola"
+
+
+
+const creditCardNumber = '4917 6100 0000 0000'
+const creditCardDate = '03/30'
+const creditCardCvc = '737'
+
+const ssn = "blabla"
+
+
+logger.debug(
+          'credit-card'
+        )
